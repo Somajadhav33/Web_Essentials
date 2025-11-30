006964e2b72a36a625540c7de70f7238438cf099
Soma <jadhavsomnath7005@gmail.com>
2025-11-30 18:36:28 +0530

    add web-socket

--- Web-Sockets/Client/src/App.jsx
+++ Web-Sockets/Client/src/App.jsx
@@ -1,20 +1,93 @@
-import React from 'react'
-import { useEffect } from 'react';
-import {io} from "socket.io-client";
+import { useEffect, useMemo, useState } from "react";
+import { io } from "socket.io-client";
+import { Button, Container, Stack, TextField, Typography } from "@mui/material";
 
 export default function App() {
-  const socket = io("http://localhost:3000");
+  const socket = useMemo(() => io("http://localhost:3000"), []);
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketId, setId] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [roomName, setRoomName] = useState("")
 
-  useEffect(() =>{
-    socket.on("connect", () =>{
+  console.log(messages);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", { message, room });
+    setMessage("");
+  };
+  const joinRoom = (e) =>{
+    e.preventDefault() 
+    socket.emit("join-room", roomName)
+    setRoomName("")
+  }
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setId(socket.id);
       console.log(`Connected to server with ID: ${socket.id}`);
     });
-  })
 
-  socket.on('welcome', (m) =>{
-    console.log(m)
-  })
+    socket.on("welcome", (msg) => {
+      console.log(msg);
+    });
+    socket.on("recieveMsg", (data) => {
+      console.log(data);
+      setMessages((messages) => [...messages, data]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
-    <div>App</div>
-  )
+    <Container maxWidth="sm">
+      <Typography variant="p" component="div" gutterBottom>
+        Your Id : {socketId}
+      </Typography>
+      <form onSubmit={joinRoom}>
+        <h5>Join Room</h5>
+         <TextField
+          id="outlined-basics"
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+          label="Room Name"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Join
+        </Button>
+
+      </form>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="outlined-basics"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          label="Message"
+          variant="outlined"
+        />
+
+        <TextField
+          id="outlined-basics"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          label="Room"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Send
+        </Button>
+      </form>
+      <Stack>
+        {messages.map((m, i) => (
+          <Typography key={i} variant="p" component="div" gutterBottom>
+            {m}
+          </Typography>
+        ))}
+      </Stack>
+    </Container>
+  );
 }
--- Web-Sockets/Server/app.js
+++ Web-Sockets/Server/app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import { Server } from 'socket.io';
-import { createServer } from 'http';
-import cors from 'cors';
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
@@ -9,23 +9,37 @@ const PORT = 3000;
 app.use(cors());
 
 const io = new Server(server, {
-    cors:{
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
-        credentials: true
-    }
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.use((socket, next) =>{
+  if(true) next();
+})
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("message", (data) =>{
+    console.log(data)
+    io.to(data.room).emit("recieveMsg", data.message)
+  })
+  socket.on("join-room", (room) =>{
+    socket.join(room)
+  })
+  socket.on("disconnect", () => {
+    console.log(`user dissconnect ${socket.id}`);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express Server");
 });
 
 
-io.on('connection', (socket) =>{
-    console.log(`User connected: ${socket.id}`);
-    socket.emit("welcome", `Welcome to the server ${socket.id}`)
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.get('/', (req, res) =>{
-    res.send('Hello from Express Server');
-})
-
-server.listen(PORT, () =>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
